6ae36f48e9ee3f8b265e1b97b043959537ba1345
Toheed <mtoheedrao@gmail.com>
2024-03-24 14:55:40 +0300

    new design folder

---
+++ lib/ui/constants/app_strings.dart
@@ -0,0 +1,6 @@
+class AppStrings {
+  static const String appName = 'Explosive App';
+  static const String welcomeMessage = 'Welcome to the explosive app';
+  static const String signInWelcome = 'Welcome back!';
+  static const String signInEnterDetail = 'Please enter details to sign in.';
+}
---
+++ lib/ui/widgets/auth_button.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AuthButton extends StatelessWidget {
+  const AuthButton({super.key});
+
+  final String title;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
